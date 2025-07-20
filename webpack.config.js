6d06e18c7af15324edc85ebdac4261b43da56777
Meargteame <mearegteame99995555@gmail.com>
2025-07-20 19:16:53 +0300

    chore: setup Webpack and Babel for React app

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+import path from 'path';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+
+export default {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve('dist'),
+    filename: 'bundle.js',
+    clean: true
+  },
+  devServer: {
+    static: './dist',
+    port: 3000,
+    open: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: 'babel-loader'
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html'
+    })
+  ]
+};
